40c40b1bd72e8b9406eafcca78b3e1a1c071a897
kamille <k.pagibigan@gmail.com>
2021-03-04 17:21:42 -0700

    edit seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 test_notebook = Notebook.create(title: "Test")
 
-note1 = Note.create(title: "Test Note", content: "This is a test note.", date: Date.today, notebook_id: 5)
+note1 = Note.create(title: "Test Note", content: "This is a test note.", date: Date.today, notebook_id: test_notebook.id)
 
-note2 = Note.create(title: "Another Test Note", content: "This is another test note. I hope everything works.", date: Date.today, notebook_id: 5)
+note2 = Note.create(title: "Another Test Note", content: "This is another test note. I hope everything works.", date: Date.today, notebook_id: test_notebook.id)
 
 another_notebook = Notebook.create(title: "Another Notebook")
 
-test_note = Note.create(title: "For Project", content: "For project notes.", date: Date.today, notebook_id: 6)
+test_note = Note.create(title: "For Project", content: "For project notes.", date: Date.today, notebook_id: another_notebook.id)
 
-test_note2 = Note.create(title: "Rails/Redux Project", content: "All my project notes are here.", date: Date.today, notebook_id: 6)
+test_note2 = Note.create(title: "Rails/Redux Project", content: "All my project notes are here.", date: Date.today, notebook_id: another_notebook.id)
