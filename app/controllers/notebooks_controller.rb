cad7f234b6374c9624d40590a4126ff9a0c70c90
kamille <k.pagibigan@gmail.com>
2021-03-04 17:49:18 -0700

    add methods to notebooks controller and notes controller

--- app/controllers/notebooks_controller.rb
+++ app/controllers/notebooks_controller.rb
@@ -1,2 +1,26 @@
 class NotebooksController < ApplicationController
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def notebook_params
+        params.require(:notebook).permit(:title)
+    end
+
+
 end
--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,26 @@
 class NotesController < ApplicationController
+
+    def index
+
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def destroy
+
+    end
+
+    private
+
+    def note_params
+        params.require(:note).permit(:title, :content, :date, :notebook_id)
+    end
+
+
 end
