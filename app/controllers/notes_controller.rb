0d88248f167d634f953d52936781afd5bbe5f3a6
kamille <k.pagibigan@gmail.com>
2021-03-19 06:14:31 -0600

    update destroy method in Notes Controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -25,7 +25,8 @@ class NotesController < ApplicationController
     end
 
     def destroy
-        note = Note.find(params[:id])
+        # note = Note.find(params[:notebook_id][:id])
+        note = Notebook.find(params[:notebook_id]).notes.find(params[:id])
         note.destroy
     end
 
